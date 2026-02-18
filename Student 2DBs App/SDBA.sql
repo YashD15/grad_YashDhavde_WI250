e6fef22237616e7b5d610b4ae2de4d2c9ec8a153
Yash Dhavde <118428757+YashD15@users.noreply.github.com>
2026-02-18 16:38:02 +0530

    Add files via upload

---
+++ Student 2DBs App/SDBA.sql
@@ -0,0 +1,8 @@
+-- CREATE DATABASE StudentDBs;
+
+CREATE TABLE students(rollno INT PRIMARY KEY, name TEXT, standard INT, school TEXT);
+
+DROP TABLE students;
+TRUNCATE TABLE students;
+
+SELECT * FROM students;
