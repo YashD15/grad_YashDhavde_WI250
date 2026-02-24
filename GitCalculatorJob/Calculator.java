799c9ddaaa0b9d9e69fe49b0540ad75c16191dc2
Yash Dhavde <yashdhavde1511@gmail.com>
2026-02-24 16:49:33 +0530

    Test

---
+++ GitCalculatorJob/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+    public int add(int a, int b) {
+        return a+b;
+    }
+    public double sub(double a, double b) {
+        return a-b;
+    }
+    public double mul(double a, double  b) {
+        return a*b;
+    }
+    public double div(double a, double b) {
+        return a/b;
+    }
+}
---
+++ GitCalculatorJob/CalculatorMain.java
@@ -0,0 +1,10 @@
+public class CalculatorMain {
+    public static void main(String args[]) {
+        System.out.println("------GitHub Calculator Job--------");
+        Calculator c1 = new Calculator();
+        System.out.println("Addition 10+20="+c1.add(10, 20));
+        System.out.println("Subtraction 50+20="+c1.add(50, 20));
+        System.out.println("Multiplication 10*20="+c1.add(10, 20));
+        System.out.println("Division 100+20="+c1.add(100, 20));
+    }
+}
